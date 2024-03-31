4cc41bb83d1768048353f86d16433bd2e00756a0
vojbob <vojtab42@gmail.com>
2024-03-31 15:47:29 +0200

    Vyresena kontrola cisel v hornim radku, vyresene vytvareni cisel ze vstupu, vyresene zapocitavani cisel

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import re
 
 matrix = []
 
+activationChars = set()
+
+def findSpecialCharsInInput():
+    specialChars= set()
+    noSpecialChar={".","\r","\n"}
+
+    for line in sys.stdin:            
+        for lineChar in line:
+            if lineChar in noSpecialChar:pass
+            elif lineChar.isdigit():pass
+            else: specialChars.add(lineChar)
+    
+    return specialChars
+
 def fillMatrix():
     i=0
     inputChunk = []
@@ -21,18 +35,23 @@ def fillMatrix():
                     break
             i+=1
 
-def firstRowSolve(row):
-    numberToCast=""
+#deprecated
+def firstRowSolve(line,matrix):
+    numberToCastToInt=""
     countNumber=False
-    for i in row:
+    for i in matrix[line][i]:
         if i == ".": #no char
             pass
 
         elif i.isdigit(): #number
-            numberToCast += row[i]
+            if True:
+            numberToCastToInt += matrix[line][i]
 
-            if row[i+1].isdigit():
+            if matrix[i+1].isdigit():
+                
                 pass
+            else:
+                countNumber=True
 
         else: #special char
             pass
@@ -40,14 +59,53 @@ def firstRowSolve(row):
 
     pass
 
+def checkTop(matrix, x, y):
+    pass
+def checkLeft(matrix, x, y):
+    pass
+def checkBottom(matrix, x, y):
+    pass
+def checkRight(matrix, x, y):
+    pass
+
+def checkSpecialCharsAroundNumber(matrix, x, y):
+    
+    #top left corner
+    if x==0 and y==0:
+        # >
+        if matrix[x+1][y] in activationChars:return True
+        # v
+        if matrix[x][y+1] in activationChars:return True
+    #top mid line
+    elif x > 0 and x < len(matrix[x]) and y==0:
+        pass
+    #top right corner
+    elif x > 0 and x == len(matrix[x])-1 and y==0:
+        pass
+    #left mid 
+    elif x==0 and y > 0 and y < len(matrix)-1:
+        pass
+    #mid
+    elif x > 0 and x < len(matrix)-1 and y > 0 and y < len(matrix)-1:
+        pass
+    #right mid 
+    elif x == len(matrix)-1 and y > 0 and y < len(matrix)-1:
+        pass
+    #bot left
+    elif x == 0 and y == len(matrix)-1:
+        pass
+
+    #bot mid
+    elif x > 0 and x < len(matrix)-1 and y == len(matrix)-1:
+        pass
+
+    #bot right
+    elif x == len(matrix)-1 and y == len(matrix)-1:
+        pass
+
 
 def solve():
     fillMatrix()
-
-    firstRow=True
-    lastRow=False
-    firstChar=True
-    lastChar=False
 
     counter=0
     
@@ -55,8 +113,9 @@ def solve():
     for i in range(matrix):
 
         if firstRow:
-            firstRowSolve(matrix)
-            firstRow=
+            firstRowSolve(i, matrix)
+            firstRow=False
+        
 
 
 
--- test.py
+++ test.py
@@ -17,52 +17,87 @@ def checkSpecialCharsAroundNumber(matrix, x, y):
     #top left corner
     if x==0 and y==0:
         # >
-        if matrix[x+1][y] in activationChars:specialCharAround += "Right"
+        if matrix[y][x+1] in activationChars:specialCharAround += "Right"
         # v
-        if matrix[x][y+1] in activationChars:specialCharAround += "Down"
+        if matrix[y+1][x] in activationChars:specialCharAround += "Down"
          # v->
-        if matrix[x+1][y+1] in activationChars:specialCharAround += "Downright"
+        if matrix[y+1][x+1] in activationChars:specialCharAround += "Downright"
     #top mid line
     elif x > 0 and x < len(matrix[x])-1 and y==0:
         # >
-        if matrix[x+1][y] in activationChars:specialCharAround += "Right"
+        if matrix[y][x+1] in activationChars:specialCharAround += "Right"
         # v->
-        if matrix[x+1][y+1] in activationChars:specialCharAround += "Downright"
+        if matrix[y+1][x+1] in activationChars:specialCharAround += "Downright"
         # v
-        if matrix[x][y+1] in activationChars:specialCharAround += "Down"
+        if matrix[y+1][x] in activationChars:specialCharAround += "Down"
         # <-v
-        if matrix[x-1][y+1] in activationChars:specialCharAround += "Downleft"
+        if matrix[y+1][x-1] in activationChars:specialCharAround += "Downleft"
         # <
-        if matrix[x-1][y] in activationChars:specialCharAround += "Left"
+        if matrix[y][x-1] in activationChars:specialCharAround += "Left"
     #top right corner
     elif x > 0 and x == len(matrix[x])-1 and y==0:
         # v
-        if matrix[x][y+1] in activationChars:specialCharAround += "Down"
+        if matrix[y+1][x] in activationChars:specialCharAround += "Down"
         # <-v
-        if matrix[x-1][y+1] in activationChars:specialCharAround += "Downleft"
+        if matrix[y+1][x-1] in activationChars:specialCharAround += "Downleft"
         # <
-        if matrix[x-1][y] in activationChars:specialCharAround += "Left"
+        if matrix[y][x-1] in activationChars:specialCharAround += "Left"
     return specialCharAround
+
+
+def vypis_pole(pole):
+  """
+  Funkce vypíše dvourozměrné pole na konzoli.
+
+  Argumenty:
+    pole: Dvourozměrné pole.
+  """
+  for radek in pole:
+    for sloupec in radek:
+      print(f"{sloupec:2}", end=" ")
+    print()
 
 y=0
 
 ar = [
-    ['2','#','3','.'],
+    ['2','3','3','#'],
     ['.','.','.','.'],
     ['.','.','.','.'],
-    ['.','.','.','.'],
+    ['2','2','.','.'],
 ]
-
+counter=0
+countNumber=False
+numberToConvert=""
 for x in range(len(ar[y])):
-    specChar=checkSpecialCharsAroundNumber(ar,x,y)
-    counter=0
-    print("[" + str(x) + "," + str(y) + "] - " +  specChar)
-    print(ar[x][y])
-    if len(specChar)>0 and ar[x][y].isdigit():
-        counter+=int(ar[x][y])
-        print("counter = " + str(counter))
-        ar[x][y]=0
-print("prehozene X a Y v pruchodu")
+    
+    print(ar[y][x])
+
+    if ar[y][x].isdigit():
+
+        specChar=checkSpecialCharsAroundNumber(ar,x,y)
+        print("[" + str(x) + "," + str(y) + "] - " +  specChar)
+
+        if len(specChar)>0: countNumber=True
+        numberToConvert+=str(ar[y][x])
+
+        if ar[y][x+1].isdigit():
+            pass
+        else:
+            if countNumber:
+                #pokud zapocitame cislo tak ho v pruchozi matici vynulujeme aby nevznikaly duplicity v souctu
+                for z in range(len(numberToConvert)):
+                    ar[y][x-z]=0
+
+                counter+=int(numberToConvert)
+                numberToConvert=""
+                specChar=""
+                countNumber=False
+                print("counter = " + str(counter))
+            else:
+                print("number wasnt around special char")
+
+                
+vypis_pole(ar)
 
 
     
